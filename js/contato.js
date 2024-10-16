8c043dd3f30ab6436484e70b421493abc2306352
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2024-10-16 11:53:12 -0300

    Uso da API Formspree para formulários

--- js/contato.js
+++ js/contato.js
@@ -1,6 +1,7 @@
 /* Selecionando os elementos que serão manipulados */ 
 const formulario = document.querySelector("form");
 const campoCep = formulario.querySelector("#cep");
+const campoTelefone = formulario.querySelector("#telefone");
 const campoEndereco = formulario.querySelector("#endereco");
 const campoBairro = formulario.querySelector("#bairro");
 const campoCidade = formulario.querySelector("#cidade");
@@ -8,8 +9,14 @@ const campoEstado = formulario.querySelector("#estado");
 const botaoBuscar = formulario.querySelector("#buscar");
 const mensagemStatus = formulario.querySelector("#status");
 
+/* Ativação das máscara para Telefone e CEP */
+$(campoTelefone).mask("(00) 0000-0000"); 
+$(campoCep).mask("00000-000"); 
+
+
+
 /* Capturando o clique no botão Buscar */
-botaoBuscar.addEventListener("click", function(){
+botaoBuscar.addEventListener("click",  async function(){
     /* Verifando se o cep NÃO TEM 9 DIGÍTOS */ 
     if( campoCep.value.length !== 9 ){
         // Informar o usuário sobre o erro
@@ -23,8 +30,92 @@ botaoBuscar.addEventListener("click", function(){
 
     /* Guardando o valor do cep digitado */ 
     let cepDigitado = campoCep.value;
+    console.log(cepDigitado);
+    
     
     /* AJAX - Asyncronous JavaScript AND XML 
-    Técnica de comunicação usada entre sistemas e tecnologia diferentes. */ 
+    Técnica de comunicação assíncrona (transmissão, recebimento) de dados muito usada entre sistemas e tecnologias diferentes. */ 
 
- }); // final do evento do botão
+    // Etapa 1: preparar a url contendo o CEP a ser buscado
+    let url = `https://viacep.com.br/ws/${cepDigitado}/json/`;
+    console.log(url);
+    
+    
+    // Etapa 2: acessar a API (com a URL) e aguardar o retorno dela
+    const resposta = await fetch(url);
+    console.log(resposta);
+    
+
+    // Etapa 3: extrair os dados do retorno/resposta
+    const dados = await resposta.json();
+    console.log(dados);
+    
+
+    // Etapa 4: lidar com os dados (em caso de erro e sucesso)
+    if( "erro" in dados ){
+        mensagemStatus.innerHTML = "CEP inexistente 😥";
+        mensagemStatus.style.color = "red";
+    } else {
+        mensagemStatus.innerHTML = "CEP encontrado 😃";
+        mensagemStatus.style.color = "blue";
+
+        // Seleciona todos os campos com a classe indicada
+        const campos = formulario.querySelectorAll(".campos-restantes");
+
+        /*  Loop/Laço de Repetição para acessar CADA campo selecionado e remover a classe fazendo com que cada campo volte a aparecer. */
+    for(const campo of campos){
+        campo.classList.remove("campos-restantes");
+    }
+    }
+
+    
+
+    /* Atribuindo os dados para cada campo */
+    
+    // Colocar o logradouro como valor do campo endereço
+    campoEndereco.value = dados.logradouro;
+
+    // Colocar o bairro como valor do campo bairro
+    campoBairro.value = dados.bairro;
+
+    // Colocar a localidade como valor do campo cidade
+    campoCidade.value = dados.localidade;
+
+    // Colocar a uf como valor do campo cidade
+    campoEstado.value = dados.uf;
+
+
+}); // final do evento do botão
+
+/* Código do Formspree */ 
+
+var form = document.getElementById("my-form");
+  
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Obrigado! Mensagem enviada com sucesso. Aguarde nosso retorno.";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! Algo de errado não está certo... tente novamente mais tarde."
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! Houve um erro... fale com o administrador pelo email <a href='mailto:admin@simplicity.com.br'>admin@simplicity.com.br</a>"
+  });
+}
+form.addEventListener("submit", handleSubmit)
